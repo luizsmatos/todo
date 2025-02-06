9291498220f325eba2b37a63eda440b8b638df6f
Luiz Gustavo <gusstavopdm@gmail.com>
2025-02-06 18:30:35 -0300

    feat(create-task.controller.ts): implement CreateTaskController to handle task creation requests and return task details

---
+++ src/modules/task/infrastructure/http/controllers/create-task.controller.ts
@@ -0,0 +1,47 @@
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { User } from '@shared/infrastructure/http/decorators/user.decorator';
+import { CreateTaskUseCase } from '@src/modules/task/application/use-cases/create-task.use-case';
+
+import {
+  CreateTaskRequestDto,
+  CreateTaskResponseDto,
+} from '../dtos/create-task.dto';
+import { CreateTaskUseCaseInput } from './../../../application/use-cases/create-task.use-case';
+import { ApiCreateTask } from './docs/create-task.swagger';
+
+@ApiTags('Tasks')
+@Controller()
+export class CreateTaskController {
+  constructor(private readonly createTaskUseCase: CreateTaskUseCase) {}
+
+  @Post('tasks')
+  @HttpCode(HttpStatus.CREATED)
+  @ApiCreateTask()
+  async handle(
+    @User() userId: string,
+    @Body() body: CreateTaskRequestDto,
+  ): Promise<CreateTaskResponseDto> {
+    const { title, description } = body;
+
+    const createTaskUseCaseInput: CreateTaskUseCaseInput = {
+      userId,
+      title,
+      description,
+    };
+
+    const { task } = await this.createTaskUseCase.execute(
+      createTaskUseCaseInput,
+    );
+
+    return new CreateTaskResponseDto({
+      id: task.id,
+      title: task.title,
+      description: task.description,
+      status: task.status.toString(),
+      userId: task.userId,
+      createdAt: task.createdAt,
+      updatedAt: task.updatedAt,
+    });
+  }
+}
