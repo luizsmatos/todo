337abbf67112290a6c8ba54645445bd9caf32c74
Luiz Gustavo <gusstavopdm@gmail.com>
2025-02-06 10:46:32 -0300

    feat(tasks.repository.ts): add pagination and filtering capabilities to tasks repository for improved task retrieval

--- src/modules/task/domain/repositories/tasks.repository.ts
+++ src/modules/task/domain/repositories/tasks.repository.ts
@@ -1,4 +1,15 @@
-import { Task } from '../entities/task.entity';
+import {
+  Pagination,
+  PaginationParams,
+} from '@shared/domain/value-objects/pagination';
+
+import { Task, TaskStatus } from '../entities/task.entity';
+
+export interface TaskFilters {
+  title?: string;
+  description?: string;
+  status?: TaskStatus;
+}
 
 export interface TasksRepository {
   create(task: Task): Promise<void>;
@@ -6,4 +17,8 @@ export interface TasksRepository {
   findById(id: string): Promise<Task | null>;
   update(task: Task): Promise<void>;
   delete(id: string): Promise<void>;
+  findManyByUserId(
+    userId: string,
+    options: { pagination: PaginationParams; filters: TaskFilters },
+  ): Promise<Pagination<Task>>;
 }
