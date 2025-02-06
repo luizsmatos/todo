32a1392c9cd96dbd0cd46581734a8b78586a2699
Luiz Gustavo <gusstavopdm@gmail.com>
2025-02-06 18:56:10 -0300

    feat(delete-task.dto.ts): create DeleteTaskRequestDto for task deletion requests to ensure proper validation and documentation

---
+++ src/modules/task/infrastructure/http/dtos/delete-task.dto.ts
@@ -0,0 +1,10 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsUUID } from 'class-validator';
+
+export class DeleteTaskRequestDto {
+  @ApiProperty({
+    example: '123e4567-e89b-12d3-a456-426655440000',
+  })
+  @IsUUID()
+  taskId: string;
+}
