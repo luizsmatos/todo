ed84f2681c3c87bbf14eafbd815cd52ddd0b9f7e
Luiz Gustavo <gusstavopdm@gmail.com>
2025-02-05 23:00:47 -0300

    feat(tasks.repository.ts): add findById and update methods to TasksRepository interface to enhance task management capabilities

--- src/modules/task/domain/repositories/tasks.repository.ts
+++ src/modules/task/domain/repositories/tasks.repository.ts
@@ -3,4 +3,6 @@ import { Task } from '../entities/task.entity';
 export interface TasksRepository {
   create(task: Task): Promise<void>;
   findByTitle(title: string): Promise<Task | null>;
+  findById(id: string): Promise<Task | null>;
+  update(task: Task): Promise<void>;
 }
