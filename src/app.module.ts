9b166ed9a085ba2b1b234f3e50ca68ceace26314
Luiz Gustavo <gusstavopdm@gmail.com>
2025-02-06 15:00:20 -0300

    feat(app.module.ts): add EnvModule to AppModule imports to manage environment configurations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 
 import { UsersModule } from './modules/user/users.module';
+import { EnvModule } from './shared/infrastructure/config/env/env.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [EnvModule, UsersModule],
   providers: [],
 })
 export class AppModule {}
