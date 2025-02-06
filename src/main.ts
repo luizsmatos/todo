ac4118a4eb2d3794d3296885af1e831779f1e702
Luiz Gustavo <gusstavopdm@gmail.com>
2025-02-06 11:49:33 -0300

    feat(main.ts): add Swagger documentation setup for API and global validation pipe to enhance API usability and input validation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,23 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Task App')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .build();
+
+  const documentFactory = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('docs', app, documentFactory);
+
+  app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(process.env.PORT ?? 3000);
 }
 
