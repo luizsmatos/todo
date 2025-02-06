404a2cfb2ea7eb8f2d9707de058a1537f14d7acd
Luiz Gustavo <gusstavopdm@gmail.com>
2025-02-06 18:24:50 -0300

    feat(app.module.ts): add TasksModule to the main application module to integrate task management functionality

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 
 import { AuthModule } from './modules/auth/auth.module';
+import { TasksModule } from './modules/task/tasks.module';
 import { UsersModule } from './modules/user/users.module';
 import { EnvModule } from './shared/infrastructure/config/env/env.module';
 import { JwtAuthGuard } from './shared/infrastructure/http/guards/jwt-auth.guard';
 
 @Module({
-  imports: [EnvModule, UsersModule, AuthModule],
+  imports: [EnvModule, UsersModule, AuthModule, TasksModule],
   providers: [
     {
       provide: 'APP_GUARD',
