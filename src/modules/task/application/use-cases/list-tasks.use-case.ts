a7c9b98a187634d79fab58688118a76b8630a756
Luiz Gustavo <gusstavopdm@gmail.com>
2025-02-06 10:53:00 -0300

    feat(list-tasks): implement ListTasksUseCase to retrieve tasks with pagination and filtering options
    This addition allows users to list tasks based on various criteria, enhancing the task management functionality.

---
+++ src/modules/task/application/use-cases/list-tasks.use-case.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@nestjs/common';
+import {
+  Pagination,
+  PaginationParams,
+} from '@shared/domain/value-objects/pagination';
+
+import { Task, TaskStatus } from '../../domain/entities/task.entity';
+import { TasksRepository } from '../../domain/repositories/tasks.repository';
+
+export interface ListTasksUseCaseInput extends Partial<PaginationParams> {
+  userId: string;
+  title?: string;
+  description?: string;
+  status?: TaskStatus;
+}
+
+export interface ListTasksUseCaseOutput {
+  tasks: Pagination<Task>;
+}
+
+@Injectable()
+export class ListTasksUseCase {
+  constructor(private readonly tasksRepository: TasksRepository) {}
+
+  async execute({
+    userId,
+    title,
+    description,
+    status,
+    page = 1,
+    pageSize = 10,
+  }: ListTasksUseCaseInput): Promise<ListTasksUseCaseOutput> {
+    const tasks = await this.tasksRepository.findManyByUserId(userId, {
+      filters: { title, description, status },
+      pagination: { page, pageSize },
+    });
+
+    return { tasks };
+  }
+}
