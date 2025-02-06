c2513d397f8bca6940771ff21d6a59b7657ef1df
Luiz Gustavo <gusstavopdm@gmail.com>
2025-02-06 16:46:19 -0300

    feat(app.module.ts): add AuthModule and JwtAuthGuard to the application module to enhance authentication capabilities

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 
+import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/user/users.module';
 import { EnvModule } from './shared/infrastructure/config/env/env.module';
+import { JwtAuthGuard } from './shared/infrastructure/http/guards/jwt-auth.guard';
 
 @Module({
-  imports: [EnvModule, UsersModule],
-  providers: [],
+  imports: [EnvModule, UsersModule, AuthModule],
+  providers: [
+    {
+      provide: 'APP_GUARD',
+      useClass: JwtAuthGuard,
+    },
+  ],
 })
 export class AppModule {}
