00d97f25ca38c0f303d0dd4a27093660a9c10406
Luiz Gustavo <gusstavopdm@gmail.com>
2025-02-06 10:11:50 -0300

    feat(pagination): add Pagination class and interface for handling paginated data
    This change introduces a new `Pagination` class and associated
    `PaginationParams` interface to manage paginated data effectively.
    This allows for better handling of pagination logic throughout the
    application, improving code organization and reusability.

---
+++ src/shared/domain/value-objects/pagination.ts
@@ -0,0 +1,31 @@
+export interface PaginationParams {
+  page: number;
+  pageSize: number;
+}
+
+interface PaginationProps<T> {
+  items: T[];
+  totalItems: number;
+  page: number;
+  pageSize: number;
+}
+
+export class Pagination<T> {
+  public readonly currentPage: number;
+  public readonly pageSize: number;
+  public readonly totalItems: number;
+  public readonly totalPages: number;
+  public readonly items: T[];
+
+  constructor({ items, totalItems, page, pageSize }: PaginationProps<T>) {
+    this.currentPage = page;
+    this.pageSize = pageSize;
+    this.totalItems = totalItems;
+    this.totalPages = Math.ceil(totalItems / pageSize);
+    this.items = items;
+  }
+
+  static create<T>(props: PaginationProps<T>): Pagination<T> {
+    return new Pagination(props);
+  }
+}
