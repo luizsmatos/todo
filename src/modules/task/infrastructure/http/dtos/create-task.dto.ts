98f45a6de35079708d6d244c8ff3971160568adc
Luiz Gustavo <gusstavopdm@gmail.com>
2025-02-06 18:24:02 -0300

    feat(create-task.dto.ts): add DTOs for creating tasks to standardize request and response formats

---
+++ src/modules/task/infrastructure/http/dtos/create-task.dto.ts
@@ -0,0 +1,53 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, MinLength } from 'class-validator';
+
+export class CreateTaskRequestDto {
+  @IsString()
+  @MinLength(3)
+  title: string;
+
+  @IsString()
+  @MinLength(3)
+  description: string;
+}
+
+export class CreateTaskResponseDto {
+  @ApiProperty({
+    example: '123e4567-e89b-12d3-a456-426655440000',
+  })
+  id: string;
+
+  @ApiProperty({
+    example: 'Task 1',
+  })
+  title: string;
+
+  @ApiProperty({
+    example: 'Task 1 description',
+  })
+  description: string;
+
+  @ApiProperty({
+    example: 'PENDING',
+  })
+  status: string;
+
+  @ApiProperty({
+    example: '123e4567-e89b-12d3-a456-426655440000',
+  })
+  userId: string;
+
+  @ApiProperty({
+    example: '2022-01-01T00:00:00.000Z',
+  })
+  createdAt: Date;
+
+  @ApiProperty({
+    example: '2022-01-01T00:00:00.000Z',
+  })
+  updatedAt: Date | null;
+
+  constructor(partial: Partial<CreateTaskResponseDto>) {
+    Object.assign(this, partial);
+  }
+}
