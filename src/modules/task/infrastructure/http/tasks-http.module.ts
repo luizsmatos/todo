a537ba78e53628f072d5ae229a2ebfea4bdbc3be
Luiz Gustavo <gusstavopdm@gmail.com>
2025-02-06 18:57:21 -0300

    feat(tasks-http.module.ts): add DeleteTaskUseCase and DeleteTaskController to handle task deletion functionality

--- src/modules/task/infrastructure/http/tasks-http.module.ts
+++ src/modules/task/infrastructure/http/tasks-http.module.ts
@@ -1,14 +1,16 @@
 import { Module } from '@nestjs/common';
 
 import { CreateTaskUseCase } from '../../application/use-cases/create-task.use-case';
+import { DeleteTaskUseCase } from '../../application/use-cases/delete-task.use-case';
 import { GetTaskUseCase } from '../../application/use-cases/get-task.use-case';
 import { TasksPersistenceModule } from '../persistence/tasks-persistence.module';
 import { CreateTaskController } from './controllers/create-task.controller';
+import { DeleteTaskController } from './controllers/delete-task.controller';
 import { GetTaskController } from './controllers/get-task.controller';
 
 @Module({
   imports: [TasksPersistenceModule],
-  controllers: [CreateTaskController, GetTaskController],
-  providers: [CreateTaskUseCase, GetTaskUseCase],
+  controllers: [CreateTaskController, GetTaskController, DeleteTaskController],
+  providers: [CreateTaskUseCase, GetTaskUseCase, DeleteTaskUseCase],
 })
 export class TasksHttpModule {}
