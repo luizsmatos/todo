f9186aa3b383e65acbea0694a9bc2035fd3ed0c0
Luiz Gustavo <gusstavopdm@gmail.com>
2025-02-06 16:36:44 -0300

    feat(auth): add DTOs for user authentication request and response to standardize data structure and improve validation

---
+++ src/modules/auth/infrastructure/http/dtos/authenticate-user.dto.ts
@@ -0,0 +1,27 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class AuthenticateUserRequestDto {
+  @ApiProperty({
+    example: 'Ttqgj@example.com',
+    required: true,
+  })
+  @IsEmail()
+  email: string;
+
+  @ApiProperty({
+    example: '123456',
+    required: true,
+  })
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+}
+
+export class AuthenticateUserResponseDto {
+  @ApiProperty({
+    example:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNT',
+  })
+  accessToken: string;
+}
