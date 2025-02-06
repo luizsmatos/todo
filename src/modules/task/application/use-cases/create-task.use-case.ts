ca7c88884d122c512094aa085df92c1f107604f5
Luiz Gustavo <gusstavopdm@gmail.com>
2025-02-05 22:03:42 -0300

    feat(create-task): implement CreateTaskUseCase to handle task creation logic and ensure task uniqueness

---
+++ src/modules/task/application/use-cases/create-task.use-case.ts
@@ -0,0 +1,40 @@
+import { Task, TaskStatus } from '../../domain/entities/task.entity';
+import { TaskAlreadyExistsException } from '../../domain/exceptions/task-already-exists.exception';
+import { TasksRepository } from '../../domain/repositories/tasks.repository';
+
+export interface CreateTaskUseCaseInput {
+  userId: string;
+  title: string;
+  description: string;
+}
+
+export interface CreateTaskUseCaseOutput {
+  task: Task;
+}
+
+export class CreateTaskUseCase {
+  constructor(private readonly tasksRepository: TasksRepository) {}
+
+  async execute({
+    userId,
+    title,
+    description,
+  }: CreateTaskUseCaseInput): Promise<CreateTaskUseCaseOutput> {
+    const taskWithSameTitle = await this.tasksRepository.findByTitle(title);
+
+    if (taskWithSameTitle) {
+      throw new TaskAlreadyExistsException(title);
+    }
+
+    const task = new Task({
+      userId,
+      title,
+      description,
+      status: TaskStatus.PENDING,
+    });
+
+    await this.tasksRepository.create(task);
+
+    return { task };
+  }
+}
