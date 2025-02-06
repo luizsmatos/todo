40940339dea94c5856ed26cacc021be0110945d0
Luiz Gustavo <gusstavopdm@gmail.com>
2025-02-05 22:15:07 -0300

    test(create-task.use-case): add unit tests for CreateTaskUseCase to ensure task creation functionality works as expected
    This commit introduces a new test file for the CreateTaskUseCase,
    verifying that tasks can be created successfully and that
    duplicate tasks are not allowed. This enhances the test coverage
    and ensures the reliability of the task creation logic.

---
+++ src/modules/task/application/use-cases/create-task.use-case.spec.ts
@@ -0,0 +1,42 @@
+import { mockCreateTask } from '../../__task__/fixtures/mock-create-task';
+import { InMemoryTasksRepository } from '../../__task__/repositories/in-memory-tasks.repository';
+import { TaskStatus } from '../../domain/entities/task.entity';
+import { TaskAlreadyExistsException } from '../../domain/exceptions/task-already-exists.exception';
+import { CreateTaskUseCase } from './create-task.use-case';
+
+let sut: CreateTaskUseCase;
+let inMemoryTasksRepository: InMemoryTasksRepository;
+
+describe('CreateTaskUseCase', () => {
+  beforeEach(() => {
+    inMemoryTasksRepository = new InMemoryTasksRepository();
+    sut = new CreateTaskUseCase(inMemoryTasksRepository);
+  });
+
+  it('should be able to create a new task', async () => {
+    const createTask = mockCreateTask();
+
+    const { task } = await sut.execute(createTask);
+
+    expect(task).toEqual({
+      id: expect.any(String) as string,
+      title: createTask.title,
+      description: createTask.description,
+      status: TaskStatus.PENDING,
+      userId: createTask.userId,
+      createdAt: expect.any(Date) as Date,
+      updatedAt: null,
+    });
+    expect(inMemoryTasksRepository.items).toHaveLength(1);
+  });
+
+  it('should not be able to create a new task with the same title', async () => {
+    const createTask = mockCreateTask();
+
+    await sut.execute(createTask);
+
+    await expect(sut.execute(createTask)).rejects.toEqual(
+      new TaskAlreadyExistsException(createTask.title),
+    );
+  });
+});
