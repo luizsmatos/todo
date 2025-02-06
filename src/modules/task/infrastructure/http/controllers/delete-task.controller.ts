224f16b996a5115205e5b71d2555917905898f5a
Luiz Gustavo <gusstavopdm@gmail.com>
2025-02-06 18:57:07 -0300

    feat(delete-task.controller): implement DeleteTaskController to handle task deletion requests
    This controller provides an endpoint to delete tasks by their ID, enhancing the task management functionality in the application.

---
+++ src/modules/task/infrastructure/http/controllers/delete-task.controller.ts
@@ -0,0 +1,37 @@
+import {
+  Controller,
+  Delete,
+  HttpCode,
+  HttpStatus,
+  Param,
+} from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { User } from '@shared/infrastructure/http/decorators/user.decorator';
+import { DeleteTaskUseCase } from '@src/modules/task/application/use-cases/delete-task.use-case';
+
+import { DeleteTaskRequestDto } from '../dtos/delete-task.dto';
+import { DeleteTaskUseCaseInput } from './../../../application/use-cases/delete-task.use-case';
+import { ApiDeleteTask } from './docs/delete-task.swagger';
+
+@ApiTags('Tasks')
+@Controller()
+export class DeleteTaskController {
+  constructor(private readonly deleteTaskUseCase: DeleteTaskUseCase) {}
+
+  @Delete('tasks/:taskId')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @ApiDeleteTask()
+  async handle(
+    @User() userId: string,
+    @Param() params: DeleteTaskRequestDto,
+  ): Promise<void> {
+    const { taskId } = params;
+
+    const deleteTaskUseCaseInput: DeleteTaskUseCaseInput = {
+      userId,
+      taskId,
+    };
+
+    await this.deleteTaskUseCase.execute(deleteTaskUseCaseInput);
+  }
+}
