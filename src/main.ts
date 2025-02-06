7fd976fa33ff10dd1ad79acdc010eed7429adbf1
Luiz Gustavo <gusstavopdm@gmail.com>
2025-02-06 16:46:07 -0300

    feat(main.ts): integrate EnvService to retrieve PORT configuration for application startup

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,12 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
+import { EnvService } from './shared/infrastructure/config/env/env.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const envService = app.get(EnvService);
 
   const swaggerConfig = new DocumentBuilder()
     .setTitle('Task App')
@@ -18,7 +21,7 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(envService.get('PORT'));
 }
 
 void bootstrap();
