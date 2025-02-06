ad9e83bc83bf4f0486d21d3ffea78410f249f97d
Luiz Gustavo <gusstavopdm@gmail.com>
2025-02-06 18:48:54 -0300

    feat(tasks-http.module.ts): add GetTaskUseCase and GetTaskController to handle task retrieval functionality

--- src/modules/task/infrastructure/http/tasks-http.module.ts
+++ src/modules/task/infrastructure/http/tasks-http.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 
 import { CreateTaskUseCase } from '../../application/use-cases/create-task.use-case';
+import { GetTaskUseCase } from '../../application/use-cases/get-task.use-case';
 import { TasksPersistenceModule } from '../persistence/tasks-persistence.module';
 import { CreateTaskController } from './controllers/create-task.controller';
+import { GetTaskController } from './controllers/get-task.controller';
 
 @Module({
   imports: [TasksPersistenceModule],
-  controllers: [CreateTaskController],
-  providers: [CreateTaskUseCase],
+  controllers: [CreateTaskController, GetTaskController],
+  providers: [CreateTaskUseCase, GetTaskUseCase],
 })
 export class TasksHttpModule {}
