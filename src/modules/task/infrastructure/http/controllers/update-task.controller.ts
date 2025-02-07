b671b7aa7c548a125d139ba6d0caae5d2c940b77
Luiz Gustavo <gusstavopdm@gmail.com>
2025-02-06 23:03:06 -0300

    feat(update-task.controller): implement UpdateTaskController to handle task updates via HTTP PUT requests
    This controller allows users to update existing tasks by providing the task ID and new details, enhancing the task management functionality in the application.

---
+++ src/modules/task/infrastructure/http/controllers/update-task.controller.ts
@@ -0,0 +1,61 @@
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Put,
+} from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { User } from '@shared/infrastructure/http/decorators/user.decorator';
+import {
+  UpdateTaskUseCase,
+  UpdateTaskUseCaseInput,
+} from '@src/modules/task/application/use-cases/update-task.use-case';
+
+import {
+  UpdateTaskBodyDto,
+  UpdateTaskParamsDto,
+  UpdateTaskResponseDto,
+} from '../dtos/update-task.dto';
+import { ApiUpdateTask } from './docs/update-task.swagger';
+
+@ApiTags('Tasks')
+@Controller()
+export class UpdateTaskController {
+  constructor(private readonly updateTaskUseCase: UpdateTaskUseCase) {}
+
+  @Put('tasks/:taskId')
+  @HttpCode(HttpStatus.OK)
+  @ApiUpdateTask()
+  async handle(
+    @User() userId: string,
+    @Param() params: UpdateTaskParamsDto,
+    @Body() body: UpdateTaskBodyDto,
+  ): Promise<UpdateTaskResponseDto> {
+    const { taskId } = params;
+    const { title, description, status } = body;
+
+    const updateTaskUseCaseInput: UpdateTaskUseCaseInput = {
+      userId,
+      taskId,
+      title,
+      description,
+      status,
+    };
+
+    const { task } = await this.updateTaskUseCase.execute(
+      updateTaskUseCaseInput,
+    );
+
+    return new UpdateTaskResponseDto({
+      id: task.id,
+      title: task.title,
+      description: task.description,
+      status: task.status.toString(),
+      userId: task.userId,
+      createdAt: task.createdAt,
+      updatedAt: task.updatedAt,
+    });
+  }
+}
