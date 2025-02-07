0e181361f4f6c5e31d5f3543abd8f903ed47b94d
Luiz Gustavo <gusstavopdm@gmail.com>
2025-02-06 23:25:59 -0300

    feat(main.ts): add Bearer authentication support to Swagger documentation for enhanced security features

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,7 @@ async function bootstrap() {
     .setTitle('Task App')
     .setDescription('API description')
     .setVersion('1.0')
+    .addBearerAuth()
     .build();
 
   const documentFactory = SwaggerModule.createDocument(app, swaggerConfig);
